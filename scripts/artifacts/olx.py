4ce5d37c1169f4348893dbfcdc83beb2479cf2dd
Twi5teR <vit.kolesnyk@gmail.com>
2025-03-09 22:53:57 +0200

    update

--- scripts/artifacts/olx.py
+++ scripts/artifacts/olx.py
@@ -8,68 +8,70 @@ __artifacts_v2__ = {
         "requirements": "none",
         "category": "OLX Selling",
         "notes": "",
-        "paths": ('*/ua.slando/databases/ChatSellingDb.db',),
+        "paths": ('*/ua.slando/databases/ChatSellingDb.db*',),
         "function": "get_olx_selling"
     }
 }
 
-from os.path import dirname, join
 import sqlite3
 
 from scripts.artifact_report import ArtifactHtmlReport
 from scripts.ilapfuncs import logfunc, tsv, timeline, open_sqlite_db_readonly, does_table_exist
 
-
 def get_olx_selling(files_found, report_folder, seeker, wrap_text, time_offset):
     # data_list1 = []
     logfunc("Processing data for OLX Selling")
-    files_found = [x for x in files_found if not x.endswith('wal') and not x.endswith('shm')]
-    file_found = str(files_found[0])
-    db = open_sqlite_db_readonly(file_found)
-    cursor = db.cursor()
-    cursor.execute('''
-    SELECT
-    datetime(`timestamp`, 'unixepoch') AS created_time,
-    `id`,
-    `ad`,
-    `respondent`,
-    `messages`
-        from ChatListItem
-      /* where id = respondent_id and json_valid(respondent) = 1 order by created_time /*
-        ''')
-
-    all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
     data_list = []
+    for file_found in files_found:
+        file_found = str(file_found)
+        if file_found.endswith('db'):
+            logfunc(f"Files found: {files_found}")
+            logfunc(f"Using database file: {file_found}")
+            db = open_sqlite_db_readonly(file_found)
+            cursor = db.cursor()
+            logfunc("Executing SQL query...")
+            cursor.execute('''
+            SELECT 
+            ChatListItem.ID,
+            ChatListItem.AD,
+            ChatListItem.RESPONDENT,
+            ChatListItem.MESSAGES
+            FROM ChatListItem
+            ORDER BY ID ASC''')
+            logfunc("SQL query executed successfully")
+            all_rows = cursor.fetchall()
+            logfunc(f"Found Rows: {len(all_rows)}")
+            #cursor.execute("PRAGMA table_info(ChatListItem);")
+            #columns = cursor.fetchall()
+            #logfunc(f"Columns in ChatListItem: {columns}")
+            usageentries = len(all_rows)
+            if usageentries > 0:
+                logfunc(f"Found OLX {usageentries} Entries")
+                report = ArtifactHtmlReport('OLX Selling')
 
-    if usageentries > 0:
-        logfunc(f"Found OLX {usageentries} Entries")
-        for row in all_rows:
-            data_list.append((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13]))
+            for row in all_rows:
+                data_list.append((row[0], row[1], row[2], row[3]))
+                db.close()
+    if data_list:
+                    report = ArtifactHtmlReport('OLX Selling')
+                    report.start_artifact_report(report_folder, 'Selling Events')
+                    report.add_script()
+                    data_headers = ('ID', 'Ad ID', 'Respondent Name', 'Message ID')
 
-        report = ArtifactHtmlReport('OLX Selling')
-        report.start_artifact_report(report_folder, 'OLX - Selling')
-        report.add_script()
-        data_headers = (
-            'Timestamp', 'ID', 'Ad ID', 'Ad Title', 'Ad Category', 'Ad Active', 'Respondent ID', 'Respondent Name',
-            'Respondent Type', 'Blocked', 'Message ID', 'Message UID', 'Message Created At', 'Message Text')
-        report.write_artifact_data_table(data_headers, data_list, file_found)
-        report.end_artifact_report()
+                    report.write_artifact_data_table(data_headers, data_list, file_found)
+                    report.end_artifact_report()
 
-        tsvname = 'OLX Selling'
-        tsv(report_folder, data_headers, data_list, tsvname)
+                    tsvname = f'OLX Selling'
+                    tsv(report_folder, data_headers, data_list, tsvname)
 
-        tlactivity = 'OLX Selling'
-        timeline(report_folder, tlactivity, data_list, data_headers)
+                    tlactivity = f'OLX Selling'
+                    timeline(report_folder, tlactivity, data_list, data_headers)
     else:
-        logfunc('No OLX Selling available')
-
-    db.close()
-
+                    logfunc('No OLX Selling available')
 
 __artifacts__ = {
     "OLX": (
         "OLX",
-        ('ChatSellingDb.db'),
+        ('ChatSellingDb.*'),
         get_olx_selling)
 }
