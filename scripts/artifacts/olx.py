22a9fdcb9daf3f42b8c697638f05b0fe1f8acfc3
Twi5teR <vit.kolesnyk@gmail.com>
2025-03-17 01:56:48 +0200

    update

--- scripts/artifacts/olx.py
+++ scripts/artifacts/olx.py
@@ -4,7 +4,7 @@ __artifacts_v2__ = {
         "description": "Extracts data from OLX database files",
         "author": "Vitalii Kolesnyk",
         "version": "0.1",
-        "date": "2025-03-02",
+        "date": "2025-03-15",
         "requirements": "none",
         "category": "OLX Selling",
         "notes": "",
@@ -20,44 +20,41 @@ from scripts.ilapfuncs import logfunc, tsv, timeline, open_sqlite_db_readonly, d
 
 
 def get_olx_selling(files_found, report_folder, seeker, wrap_text, time_offset):
-    # data_list1 = []
     logfunc("Processing data for OLX Selling")
     data_list = []
+    data_list2 = []
     for file_found in files_found:
         file_found = str(file_found)
-        if file_found.endswith('db'):
+        if file_found.endswith('ChatSellingDb.db'):
             logfunc(f"Files found: {files_found}")
             logfunc(f"Using database file: {file_found}")
             db = open_sqlite_db_readonly(file_found)
             cursor = db.cursor()
             logfunc("Executing SQL query...")
             cursor.execute('''
-            SELECT 
-    datetime(ChatListItem.timestamp,'unixepoch'),
-    id,
-    json_extract(ad, '$.id'),  
-    json_extract(ad, '$.title'),  
-    json_extract(ad, '$.category.type'),  
-    case json_extract(ad, '$.active') 
-    when 0 then 'No'
-    when 1 then 'Yes'
-    end as "Ad Active",
-    json_extract(respondent, '$.id'),  
-    json_extract(respondent, '$.name'),  
-    json_extract(respondent, '$.type'),  
-    json_extract(respondent, '$.blocked'),  
-    json_extract(messages, '$[0].id'),  
-    json_extract(messages, '$[0].user_id'),  
-    json_extract(messages, '$[0].created_at'),  
-    json_extract(messages, '$[0].text')
-            FROM ChatListItem
-            ''')
+                    SELECT 
+            datetime(ChatListItem.timestamp,'unixepoch'),
+            id,
+            json_extract(ad, '$.id'),  
+            json_extract(ad, '$.title'),  
+            json_extract(ad, '$.category.type'),  
+            case json_extract(ad, '$.active') 
+            when 0 then 'No'
+            when 1 then 'Yes'
+            end as "Ad Active",
+            json_extract(respondent, '$.id'),  
+            json_extract(respondent, '$.name'),  
+            json_extract(respondent, '$.type'),  
+            json_extract(respondent, '$.blocked'),  
+            json_extract(messages, '$[0].id'),  
+            json_extract(messages, '$[0].user_id'),  
+            json_extract(messages, '$[0].created_at'),  
+            json_extract(messages, '$[0].text')
+                    FROM ChatListItem
+                    ''')
             logfunc("SQL query executed successfully")
             all_rows = cursor.fetchall()
             logfunc(f"Found Rows: {len(all_rows)}")
-            # cursor.execute("PRAGMA table_info(ChatListItem);")
-            # columns = cursor.fetchall()
-            # logfunc(f"Columns in ChatListItem: {columns}")
             usageentries = len(all_rows)
             if usageentries > 0:
                 logfunc(f"Found OLX {usageentries} Entries")
@@ -67,25 +64,85 @@ def get_olx_selling(files_found, report_folder, seeker, wrap_text, time_offset):
                 data_list.append((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
                                   row[10], row[11], row[12], row[13]))
                 db.close()
-    if data_list:
-        report = ArtifactHtmlReport('OLX Selling')
-        report.start_artifact_report(report_folder, 'Selling Events')
-        report.add_script()
-        data_headers = (
-        'Timestamp (UTC)', 'ID', 'Ad ID', 'Ad Title', 'Ad Category', 'Ad Active', 'Respondent ID', 'Respondent Name',
-        'Respondent Type', 'Blocked', 'Message ID', 'Message UID', 'Message Created At', 'Message Text')
 
-        report.write_artifact_data_table(data_headers, data_list, file_found)
-        report.end_artifact_report()
+        if file_found.endswith('db3'):
+            logfunc(f"Extraction from WAL found: {files_found}")
+            logfunc(f"Using database file: {file_found}")
+            db = open_sqlite_db_readonly(file_found)
+            cursor = db.cursor()
+            logfunc("Executing SQL query...")
+            cursor.execute('''
+            SELECT 
+            datetime(ChatListItem.timestamp,'unixepoch'),
+            id,
+            json_extract(ad, '$.id'),  
+            json_extract(ad, '$.title'),  
+            json_extract(ad, '$.category.type'),  
+            case json_extract(ad, '$.active') 
+            when 0 then 'No'
+            when 1 then 'Yes'
+            end as "Ad Active",
+            json_extract(respondent, '$.id'),  
+            json_extract(respondent, '$.name'),  
+            json_extract(respondent, '$.type'),  
+            json_extract(respondent, '$.blocked'),  
+            json_extract(messages, '$[0].id'),  
+            json_extract(messages, '$[0].user_id'),  
+            json_extract(messages, '$[0].created_at'),  
+            json_extract(messages, '$[0].text')
+                    FROM ChatListItem
+                    ''')
+            logfunc("SQL query executed successfully")
+            all_rows = cursor.fetchall()
+            logfunc(f"Found Rows: {len(all_rows)}")
+            usageentries = len(all_rows)
+            if usageentries > 0:
+                logfunc(f"Found OLX {usageentries} Entries")
+                report = ArtifactHtmlReport('OLX Selling')
 
-        tsvname = f'OLX Selling'
-        tsv(report_folder, data_headers, data_list, tsvname)
+            for row in all_rows:
+                data_list2.append((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
+                              row[10], row[11], row[12], row[13]))
+                db.close()
 
-        tlactivity = f'OLX Selling'
-        timeline(report_folder, tlactivity, data_list, data_headers)
-    else:
-        logfunc('No OLX Selling available')
+        if data_list:
+            report = ArtifactHtmlReport('OLX Selling')
+            report.start_artifact_report(report_folder, 'Selling Events')
+            report.add_script()
+            data_headers = (
+                'Timestamp (UTC)', 'ID', 'Ad ID', 'Ad Title', 'Ad Category', 'Ad Active', 'Respondent ID',
+                'Respondent Name',
+                'Respondent Type', 'Blocked', 'Message ID', 'Message UID', 'Message Created At', 'Message Text')
 
+            report.write_artifact_data_table(data_headers, data_list, file_found)
+            report.end_artifact_report()
+
+            tsvname = f'OLX Selling'
+            tsv(report_folder, data_headers, data_list, tsvname)
+
+            tlactivity = f'OLX Selling'
+            timeline(report_folder, tlactivity, data_list, data_headers)
+
+        if data_list2:
+            report = ArtifactHtmlReport('OLX Selling - WAL')
+            report.start_artifact_report(report_folder, 'Selling Events - WAL')
+            report.add_script()
+            data_headers = (
+                'Timestamp (UTC)', 'ID', 'Ad ID', 'Ad Title', 'Ad Category', 'Ad Active', 'Respondent ID',
+                'Respondent Name',
+                'Respondent Type', 'Blocked', 'Message ID', 'Message UID', 'Message Created At', 'Message Text')
+
+            report.write_artifact_data_table(data_headers, data_list2, file_found)
+            report.end_artifact_report()
+
+            tsvname = f'OLX Selling'
+            tsv(report_folder, data_headers, data_list2, tsvname)
+
+            tlactivity = f'OLX Selling'
+            timeline(report_folder, tlactivity, data_list2, data_headers)
+
+        else:
+            logfunc('No OLX Selling available')
 
 __artifacts__ = {
     "OLX": (
